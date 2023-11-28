159badf03c6ac2ba3eb9d3bfda0ce92edd82a403
Kacper <kacpercichy@onet.pl>
2023-11-28 10:33:33 +0100

    quick add handleLogin

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,12 +1,15 @@
 import { Box, Button, Flex } from '@chakra-ui/react';
 
 export const Auth = () => {
+    const handleLogin = () => {
+        console.log('Login button clicked!');
+    };
     return (
         <Flex direction="column" align="center" justify="center" height="80vh">
             <Box fontSize="30px" marginBottom="10">
                 Welcome! Login as a Guest below!
             </Box>
-            <Button size='lg' colorScheme='green'>Login</Button>
+            <Button size='lg' colorScheme='green' onClick={handleLogin}>Login</Button>
         </Flex>
     );
 };
