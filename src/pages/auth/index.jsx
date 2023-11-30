75b816acdffc01f85b2bedb6e7035fdf61bb8c8d
Kacper <kacpercichy@onet.pl>
2023-11-30 17:25:18 +0100

    add load animation

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,45 +1,81 @@
-import {Box, Button, Flex} from '@chakra-ui/react';
-import {useEffect, useState} from "react";
+import { Box, Button, Flex, Spinner } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+
+const getRandomTime = () => {
+    return Math.floor(Math.random() * (1300 - 700 + 1)) + 700;
+};
 
 export const Auth = () => {
-    const CLIENT_ID = '98f249abe07f4856918d1a817cd84254'
-    const REDIRECT_URI = 'http://localhost:5173/auth'
-    const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
-    const RESPONSE_TYPE = 'token'
-    const [token, setToken] = useState("")
+    const CLIENT_ID = '98f249abe07f4856918d1a817cd84254';
+    const REDIRECT_URI = 'http://localhost:5173/auth';
+    const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+    const RESPONSE_TYPE = 'token';
+    const [token, setToken] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const hash = window.location.hash
+        const hash = window.location.hash;
         console.log('Hash:', hash);
-        let token = window.localStorage.getItem("token")
+        const savedToken = window.localStorage.getItem('token');
 
-        if (!token && hash) {
-            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+        if (!savedToken && hash) {
+            const newToken = hash.substring(1).split('&').find(elem => elem.startsWith('access_token')).split('=')[1];
 
-            window.location.hash = ""
-            window.localStorage.setItem("token", token)
+            window.location.hash = '';
+            window.localStorage.setItem('token', newToken);
+            setToken(newToken);
+        } else {
+            setToken(savedToken);
         }
-        setToken(token)
-    }, [])
+
+        const randomTime = getRandomTime();
+        setTimeout(() => {
+            setLoading(false);
+        }, randomTime);
+    }, []);
 
     const handleLogin = () => {
-        window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
+        setLoading(true);
+
+        const randomTime = getRandomTime();
+        setTimeout(() => {
+            window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
+        }, randomTime);
     };
+
     const handleLogout = () => {
-        setToken('')
-        window.localStorage.removeItem('token')
+        setLoading(true);
+
+        const randomTime = getRandomTime();
+        setTimeout(() => {
+            setToken('');
+            window.localStorage.removeItem('token');
+            setLoading(false);
+        }, randomTime);
     };
+
+    if (loading) {
+        return (
+            <Flex direction="column" align="center" justify="center" height="80vh">
+                <Spinner size="xl" />
+                <Box fontSize="20px" marginTop="4">
+                    Loading...
+                </Box>
+            </Flex>
+        );
+    }
+
     return (
         <Flex direction="column" align="center" justify="center" height="80vh">
             <Box fontSize="30px" marginBottom="10">
                 {token ? 'You are already logged In!' : 'Welcome! Login to Spotify below!'}
             </Box>
             {token ? (
-                <Button size="lg" colorScheme="red" onClick={handleLogout}>
+                <Button className="logout-button" size="lg" colorScheme="red" onClick={handleLogout}>
                     Logout
                 </Button>
             ) : (
-                <Button size="lg" colorScheme="green" onClick={handleLogin}>
+                <Button className="login-button" size="lg" colorScheme="green" onClick={handleLogin}>
                     Login
                 </Button>
             )}
