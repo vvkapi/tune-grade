4a13da312148d55ee389f8f9aa968bb51bd406a0
Kacper <kacpercichy@onet.pl>
2023-12-08 19:50:53 +0100

    del typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,25 +6,15 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  settings: {
-    react: { version: '18.2' },
-  },
-  plugins: ['react-refresh', '@typescript-eslint'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-};
+}
