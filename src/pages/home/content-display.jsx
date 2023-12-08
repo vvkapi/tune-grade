8c0924a490c09b2a5eb9b86180fc65c405d63180
Kacper <kacpercichy@onet.pl>
2023-12-08 20:18:50 +0100

    playlists display

--- src/pages/home/content-display.jsx
+++ src/pages/home/content-display.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import AlbumsDisplay from "./albums-display.jsx";
 import ArtistsDisplay from "./artists-display.jsx";
 import PodcastsDisplay from "./podcasts-display.jsx";
+import PlaylistsDisplay from "./playlists-display.jsx";
 
 const ContentDisplay = ({ content, displayType }) => {
     switch (displayType) {
@@ -11,6 +12,8 @@ const ContentDisplay = ({ content, displayType }) => {
             return <ArtistsDisplay content={content} />;
         case "Podcasts":
             return <PodcastsDisplay content={content} />;
+        case "Playlists":
+            return <PlaylistsDisplay content={content} />;
         default:
             return null;
     }
